1ec9e096835b3ddd3811df9676502e0e7f73267d
嶋津温紀 <160441081@ccalumni.meijo-u.ac.jp>
2021-05-17 16:28:22 +0900

    AddComment

--- src/controller/action.ts
+++ src/controller/action.ts
@@ -1,8 +1,10 @@
+// 画面からロジックへ伝えるメッセージの型
+
 import type { Dice } from "../model/dice";
 
 export type Action = Readonly<{
-  type: "ROLL_DICE"; 
+  type: "ROLL_DICE"; // ダイスを振る
 } | {
-  type: "CHANGE_DICE";
+  type: "CHANGE_DICE"; // CHANGE_DICE
   dice: Dice;
 }>;
